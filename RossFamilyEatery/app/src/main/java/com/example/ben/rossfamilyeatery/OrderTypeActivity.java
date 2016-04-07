b1f462c3bf43095eca67fda8e90e8ff08f88cd76
Leathel <benongrant@hotmail.com>
2016-04-07 12:43:15 -0400

    First commit

---
+++ RossFamilyEatery/app/src/main/java/com/example/ben/rossfamilyeatery/OrderTypeActivity.java
@@ -0,0 +1,19 @@
+package com.example.ben.rossfamilyeatery;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class OrderTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_type);
+
+    }
+
+}
